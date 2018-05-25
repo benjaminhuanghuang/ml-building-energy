d6971e25d2ba2440338cab15179134987e0d5d8d
Benjamin Huang <bhuang@rms.com>
2018-05-25 16:59:44 -0700

    update

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 ## Reference
 - A Complete Machine Learning Project Walk-Through in Python
+    - https://github.com/WillKoehrsen/machine-learning-project-walkthrough
+    - http://www.nyc.gov/html/gbee/downloads/misc/nyc_benchmarking_disclosure_data_definitions_2017.pdf
+
+
 
 ## Objective
     The objective is to use the energy data to build a model that can predict the Energy Star Score of a building and interpret the results to find the factors which influence the score.
 
+    What is Energy Star Score?
+    - A 1-to-100 percentile ranking based on self-reported energy usage for the reporting year. The Energy Star score is a relative measure used for comparing the energy efficiency of buildings.
+
+
 ## Steps
 1. Data cleaning and formatting
+    - convert data type
+    - dealing with missing values.
+    - remove outliers
+    
 2. Exploratory data analysis
 3. Feature engineering and selection
 4. Compare several machine learning models on a performance metric
@@ -21,9 +33,10 @@ virtualenv --system-site-packages -p python3 venv3
 
 . venv3/bin/activate
 
-(venv3)$ pip3 install -r requirements.txt
+(venv3) pip3 install -r requirements.txt
 
 ...
-(venv3)$ deactivate
+(venv3) deactivate
 
 ```
+
--- building_energy.py
+++ building_energy.py
@@ -1,8 +1,85 @@
 import pandas as pd
 import numpy as np
 
-# Read in data into a dataframe 
-data = pd.read_csv('data/Energy_and_Water_Data_Disclosure_for_Local_Law_84_2017__Data_for_Calendar_Year_2016_.csv')
+
+def missing_values_table(df):
+    '''
+    Function to calculate missing values by column
+    '''
+    # Total missing values
+    mis_val = df.isnull().sum()
+
+    # Percentage of missing values
+    mis_val_percent = 100 * df.isnull().sum() / len(df)
+
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent], axis=1)
+
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+        columns={0: 'Missing Values', 1: '% of Total Values'})
+
+    # Sort the table by percentage of missing descending
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:, 1] != 0].sort_values(
+        '% of Total Values', ascending=False).round(1)
+
+    # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+           "There are " + str(mis_val_table_ren_columns.shape[0]) +
+           " columns that have missing values.")
+
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
+
+
+# Read in data into a dataframe
+data = pd.read_csv(
+    'data/Energy_and_Water_Data_Disclosure_for_Local_Law_84_2017__Data_for_Calendar_Year_2016_.csv')
 
 # Display top of dataframe
-data.head()
+print(data.head())
+
+
+# See the column data types and non-missing values
+print(data.info())
+
+
+# some of the columns that clearly contain numbers (such as ft²), are stored as object
+# Replace all occurrences of Not Available with numpy not a number
+data = data.replace({'Not Available': np.nan})
+
+# Iterate through the columns
+for col in list(data.columns):
+    # Select columns that should be numeric
+    if ('ft²' in col or 'kBtu' in col or 'Metric Tons CO2e' in col or 'kWh' in
+            col or 'therms' in col or 'gal' in col or 'Score' in col):
+        # Convert the data type to float
+        data[col] = data[col].astype(float)
+
+# Statistics for each column
+data.describe()
+
+# we will remove any columns with more than 50% missing values and remove outliers
+# Get the columns with > 50% missing
+missing_df = missing_values_table(data)
+missing_columns = list(missing_df[missing_df['% of Total Values'] > 50].index)
+print('We will remove %d columns.' % len(missing_columns))
+# Drop the columns
+data = data.drop(columns = list(missing_columns))
+# For older versions of pandas (https://github.com/pandas-dev/pandas/issues/19078)
+# data = data.drop(list(missing_columns), axis = 1)
+
+
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+# Rename the score 
+data = data.rename(columns = {'ENERGY STAR Score': 'score'})
+# Histogram of the Energy Star Score
+plt.style.use('fivethirtyeight')
+plt.hist(data['score'].dropna(), bins = 100, edgecolor = 'k')
+plt.xlabel('Score'); plt.ylabel('Number of Buildings')
+plt.title('Energy Star Score Distribution')
+plt.show()
